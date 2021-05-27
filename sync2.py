b5815a6f011750aa4cf58cea9c1213a9bd307da4
aditis <aditis@jpl.nasa.gov>
2021-05-27 01:48:08 -0700

    syncing master

---
+++ sync2.py
@@ -0,0 +1,22 @@
+import git, os, shutil
+
+# local version of airflow
+test = git.Repo('')
+# official version of airflow - cloned locally as apache-airflow
+airflow = git.Repo('../apache-airflow')
+fetch = airflow.remotes.origin.fetch()[0]
+print(fetch)
+pull = airflow.remotes.origin.pull()
+print(pull)
+# at = test.create_remote('at', "https://github.com/apache/airflow.git")
+at = test.remotes['at']
+at.fetch()
+# at.pull(at.refs[0].remote_head)
+test.git.pull("at", "master", "--allow-unrelated-histories")
+test.git.add("-A")
+test.git.commit("-m", "syncing master")
+test.git.push("origin", "test")
+
+
+print("---- DONE ----")
+
